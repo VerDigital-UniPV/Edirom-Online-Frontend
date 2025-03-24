70b09ae3ad848c36dc5204e75fa39bedba9afea2
Daniel Jettka <research@jettka.de>
2025-03-24 13:10:20 +0100

    some naming and adding new ant targets

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ while getopts "d" flag; do
     # set a variable OPTIONS with the remaining input arguments to pass to the build command
     OPTIONS=${@}
     # run docker
-    docker run --rm -it -v `pwd`:/app --name sencha ghcr.io/bwbohl/sencha-cmd:latest ./build.sh $OPTIONS
+    docker run --rm -it -v `pwd`:/app --name sencha ghcr.io/bwbohl/sencha-cmd:2.1.0 ./build.sh $OPTIONS
    exit
    ;;
    \?)
@@ -20,15 +20,16 @@ while getopts "d" flag; do
 done
 
 # Welcome
-echo "*********************************************"
-echo "* Welcome to the Edirom Online build script *"
-echo "*********************************************"
+echo "******************************************************"
+echo "* Welcome to the Edirom-Online Frontend build script *"
+echo "******************************************************"
 echo ""
 
 # set shell to exit if any command fails
 set -e
 
 # check dependencies
+echo ""
 echo "Checking dependencies…"
 echo "----------------------"
 type sencha ant ruby >&2
@@ -36,21 +37,31 @@ xargs=$(which gxargs || which xargs)
 echo ""
 
 # cleaning the build dir
+echo ""
 echo "Cleaning…"
 echo "---------"
 sencha ant clean
 
 # building the app
-echo "Building Edirom Online frontend…"
+echo ""
+echo "Building Edirom-Online Frontend…"
 echo "-----------------"
 sencha app build $1
 
-# get additional stuff for exist-db
-echo "Copying additional resources to build directory…"
+# download euryanthe font
+echo ""
+echo "Downloading Euryanthe Font…"
 echo "------------------------------------------------"
-ant build-plus
+ant download-euryanthe
+
+# inject build properties
+echo ""
+echo "Injecting build properties…"
+echo "------------------------------------------------"
+ant inject-properties
 
 # build xar
-echo "Packaging Edirom Online as XAR archive…"
+echo ""
+echo "Packaging Edirom-Online Frontend as XAR archive…"
 echo "---------------------------------------"
 ant xar
