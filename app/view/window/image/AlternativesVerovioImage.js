5a13b8aff64ea094e57b0b0e86242659756d66bf
Francesco Maccarini <francesco@wsl>
2025-11-18 15:06:09 +0100

    adapt alternative veriovio view to not use tipped.js

--- app/view/window/image/AlternativesVerovioImage.js
+++ app/view/window/image/AlternativesVerovioImage.js
@@ -43,13 +43,7 @@ Ext.define('EdiromOnline.view.window.image.AlternativesVerovioImage', {
 						src="https://code.jquery.com/jquery-3.5.1.min.js"
 						integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
 						crossorigin="anonymous"></script>
-					<script
-						src="//code.iconify.design/1/1.0.6/iconify.min.js"></script>
-					<script
-						src="resources/js/he.js"></script>
-					
-					<script
-						src="resources/js/tipped/tipped.js"></script>
+
 					<link
 						rel="stylesheet"
 						type="text/css"
@@ -70,10 +64,7 @@ Ext.define('EdiromOnline.view.window.image.AlternativesVerovioImage', {
 						<span
 							class="button"
 							onclick="prevPage()">
-							<span
-								class="iconify"
-								data-icon="mdi-chevron-left"
-								style="font-size: 1.3em;"></span>
+							<span style="font-size: 1.3em;">&lt;</span>
 						</span>
 						<span
 							id="page">1</span> / <span
@@ -81,10 +72,7 @@ Ext.define('EdiromOnline.view.window.image.AlternativesVerovioImage', {
 						<span
 							class="button"
 							onclick="nextPage()">
-							<span
-								class="iconify"
-								data-icon="mdi-chevron-right"
-								style="font-size: 1.3em;"></span>
+							<span style="font-size: 1.3em;">&gt;</span>
 						</span>
 					</div>
 					<div
--- resources/js/alternatives-verovio-view.js
+++ resources/js/alternatives-verovio-view.js
@@ -42,8 +42,19 @@ function showMovement(movementId) {
     showLoader();
     window.movementId = movementId;
 
-    const initHeight = Math.floor($(document).height() * 100.0 / 33.0) - 35;
-    const initWidth = Math.floor($(document).width() * 100.0 / 33.0);
+    var body = document.body;
+    var html = document.documentElement;
+
+    var height = Math.max( body.scrollHeight, body.offsetHeight, 
+                    html.clientHeight, html.scrollHeight, html.offsetHeight );
+    var width = Math.max( body.scrollWidth, body.offsetWidth, 
+                    html.clientWidth, html.scrollWidth, html.offsetWidth );
+    
+    var initHeight = Math.floor(height * 100.0 / 33.0) - 35;
+    var initWidth = Math.floor(width * 100.0 / 33.0);
+
+    var initHeight = Math.floor(height * 100.0 / 33.0) - 35;
+    var initWidth = Math.floor(width * 100.0 / 33.0);
 
     const options = {
         scale: 25,
@@ -68,20 +79,30 @@ function showMovement(movementId) {
 
     console.log("Current appXPath in showMovement: ", appXPath);
 
-    const url = `${appBasePath}/data/xql/getMusicInMdiv.xql?uri=${uri}&edition=${edition}&movementId=${movementId}`;
+    /* Load the file using HTTP GET */
+    var url = appBasePath + "/data/xql/getMusicInMdiv.xql?uri=" + uri + "&edition=" + edition    + "&movementId=" + movementId;
 
-    $.get(url, function (data) {
-        meiString = data;
-        meiDOM = parser.parseFromString(data, "application/xml");
+    fetch(url)
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok: ' + response.status);
+            return response.text();
+        })
+        .then(data => {
+            meiString = data;
+            meiDOM = parser.parseFromString(data, "application/xml");
 
-        saveMainToolkitState(options, data);
+            saveMainToolkitState(options, data);
 
-        const svg = vrvToolkit.renderData(data, options);
-        renderSVG(svg);
+            const svg = vrvToolkit.renderData(data, options);
+            renderSVG(svg);
 
-        retrieveApp(meiDOM);
-        initData();
-    }, 'text');
+            retrieveApp(meiDOM);
+            initData();
+        })
+        .catch(error => {
+            console.error('Error loading movement data:', error);
+            document.getElementById('output').textContent = 'Error loading movement.';
+        });
 }
 
 function saveMainToolkitState(options, data) {
@@ -155,28 +176,66 @@ function updatePageData() {
 
         measure.append(annotIcon);
         
+        // create tooltip
+        const tip = document.createElementNS("http://www.w3.org/1999/xhtml", "div");
+        tip.setAttributeNS(null, "class", "tip");
+        tip.setAttributeNS(null, "data-refs", annotId);
+        tip.style.position = 'absolute';
+        tip.style.display = 'none';
+        tip.style.height = 'auto';
+        tip.style.maxWidth = '300px';
+        tip.style.background = 'rgb(218, 218, 218)';
+        tip.style.border = '1px solid black';
+        tip.style.borderRadius = '5px';
+        tip.style.padding = '5px';
+        tip.style.zIndex = '10';
+        tip.innerHTML = "Error getting annotation.";
+
+        // do AJAX call to get annotation content with fetch
+        fetch(appBasePath + 'data/xql/getAnnotation.xql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                uri: uri + '#' + annotId,
+                target: 'tip',
+                edition: edition
+            })
+        })
+        .then(response => response.text())
+        .then(data => {
+            tip.innerHTML = data;
+        })
+        .catch(error => {
+            tip.innerHTML = "Error fetching annotation.";
+            console.error('Error fetching annotation:', error);
+        });
+
+        document.body.appendChild(tip);
+        
         annotIcon.addEventListener('click', (e) => {
             parent.loadLink(uri + '#' + annotId);
         });
-        
-        Tipped.create(annotIcon, {
-            ajax: {
-                url: appBasePath + 'data/xql/getAnnotation.xql',
-                type: 'post',
-                data: {
-                    uri: uri + '#' + annotId,
-                    target: 'tip',
-                    edition: edition
-                }
-            },
-            target: 'mouse', 
-            hideDelay: 1000,
-            skin: 'gray',
-            containment: {
-                  selector: '#output',
-                  padding: 0
-                }
+
+        annotIcon.addEventListener('mouseover', (e) => {
+            annotIcon.style.cursor = 'pointer';
+
+            // position and show tooltip
+            const bbox = annotIcon.getBoundingClientRect();
+            const tip = document.querySelector('.tip[data-refs="' + annotIcon.getAttributeNS(null, "data-id") + '"]');
+            tip.style.left = (bbox.x + window.scrollX - 20) + 'px';
+            tip.style.top = (bbox.y + window.scrollY + 20) + 'px';
+            tip.style.display = 'block';
+        });
+
+        annotIcon.addEventListener('mouseout', (e) => {
+            annotIcon.style.cursor = 'default';
+            // hide all tooltips
+            document.querySelectorAll('.tip').forEach((tip) => {
+                tip.style.display = 'none';
             });
+        });
     });
 }
 
